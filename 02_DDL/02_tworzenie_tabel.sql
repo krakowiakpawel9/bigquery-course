1c3ffcc8da2834ada79a72cfa3fe36370ef0b07c
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-10-26 16:17:13 +0200

    Update 02_tworzenie_tabel.sql

--- 02_DDL/02_tworzenie_tabel.sql
+++ 02_DDL/02_tworzenie_tabel.sql
@@ -11,3 +11,21 @@ INSERT INTO DDL.02_table VALUES (2, 'TOM', true, '1992-10-01', CURRENT_TIMESTAMP
 INSERT INTO DDL.02_table VALUES (3, 'JOHN', true, '1989-10-01', CURRENT_TIMESTAMP());
 
 SELECT * FROM DDL.02_table;
+
+-- Tworzenie tabeli z zapytania
+SELECT * FROM `bigquery-public-data.github_repos.languages` LIMIT 10;
+
+CREATE TABLE git.lang AS SELECT * FROM `bigquery-public-data.github_repos.languages` LIMIT 10;
+
+-- Kolumna wymagana
+CREATE TABLE IF NOT EXISTS DDL.03_table (
+  id INT64 NOT NULL,
+  name STRING,
+  has_married BOOL,
+  date_of_birth DATE,
+  insert_timestamp TIMESTAMP
+);
+  
+INSERT INTO DDL.03_table VALUES (1, 'Mark', false, '1990-01-01', CURRENT_TIMESTAMP());
+INSERT INTO DDL.03_table VALUES (2, 'Oleg', NULL, NULL, CURRENT_TIMESTAMP());
+INSERT INTO DDL.03_table VALUES (NULL, 'Mike', NULL, NULL, CURRENT_TIMESTAMP());
