5a7aac319e57d88b9d2bb1f3032d36c7ee573579
Paweł Krakowiak <37594623+krakowiakpawel9@users.noreply.github.com>
2019-10-27 18:14:51 +0100

    Create 07_union_all_distinct.sql

---
+++ 04_DQL/07_union_all_distinct.sql
@@ -0,0 +1,34 @@
+CREATE TABLE DQL.01_tab (
+  id STRING NOT NULL,
+  age INT64,
+  name STRING
+);
+
+-- 4 różne wiersze
+INSERT INTO DQL.01_tab VALUES ('001', 22, 'Mark');
+INSERT INTO DQL.01_tab VALUES ('002', 27, 'Paul');
+INSERT INTO DQL.01_tab VALUES ('003', 30, 'Rose');
+INSERT INTO DQL.01_tab VALUES ('004', 24, 'John');
+
+CREATE TABLE DQL.02_tab (
+  id STRING NOT NULL,
+  age INT64,
+  name STRING
+);
+
+-- jeden duplikat
+INSERT INTO DQL.02_tab VALUES ('001', 22, 'Mark');
+INSERT INTO DQL.02_tab VALUES ('001', 22, 'Mark');
+INSERT INTO DQL.02_tab VALUES ('002', 27, 'Paul');
+INSERT INTO DQL.02_tab VALUES ('005', 31, 'Tom');
+
+SELECT * FROM DQL.01_tab;
+SELECT * FROM DQL.02_tab;
+
+SELECT * FROM DQL.01_tab
+UNION ALL
+SELECT * FROM DQL.02_tab;
+
+SELECT * FROM DQL.01_tab
+UNION DISTINCT
+SELECT * FROM DQL.02_tab;
